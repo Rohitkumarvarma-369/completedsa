45aac8630f1ed2cdbbbcb91f68892b24f99052ce
Rohitkumarvarma-369 <therohitkumarvarma.codelife@gmail.com>
2022-09-18 00:29:55 +0530

    reverse_number

--- reverse_number.cpp
+++ reverse_number.cpp
@@ -5,11 +5,12 @@ int main(){
     int n;
     cin>>n;
 
-    int ans = 0;
-    while(n>0){
-        int p = n%10;
-        ans = (ans*10)+p;
+    long long int ans = 0;
+    while(n){
+        int digit = n%10;
+        if ((ans > 0 && ans > (INT_MAX - digit)/10) | (ans < 0 && ans < (INT_MIN - digit)/10)) return 0;
+        ans = (ans*10)+(digit);
         n = n/10;
     }
-    cout<<ans<<endl;
+    cout<<ans;
 }
